db595f10e378910763796220179d0ad35f8ec046
tatialveso <tatiialveso@gmail.com>
2022-02-15 14:42:09 -0300

    :sparkles: create and set home component

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+        <h1 className='title'>Home</h1>
+        <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500" height="500"/>
+    </>
+  );
+}
+
+export default Home;
