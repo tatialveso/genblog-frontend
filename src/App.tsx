0db5a1adc4c14a57da2543a843bdefb5acfc052d
tatialveso <tatiialveso@gmail.com>
2022-02-21 11:48:45 -0300

    :recycle: refactor files

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,31 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
-import Footer from './components/statics/Footer/Footer';
-import Navbar from './components/statics/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Login from './components/Login/Login';
+import Register from './components/User/Register/Register';
 
 function App() {
   return (
     <Router>
       <Navbar />
-        <Switch>
-          <div>
-            <Route path='/home'>
-              <Home />
-            </Route>
-          </div>
-        </Switch>
+      <Switch>
+        <div style={{ minHeight: '100vh' }}>
+          <Route path='/'>
+            <Login />
+          </Route>
+          <Route path='/home'>
+            <Home />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/register'>
+            <Register />
+          </Route>
+        </div>
+      </Switch>
       <Footer />
     </Router>
   );
