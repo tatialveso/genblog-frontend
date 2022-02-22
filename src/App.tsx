37ccd800da5d0fef14a634036596b961050055ce
tatialveso <tatiialveso@gmail.com>
2022-02-22 14:51:16 -0300

    :sparkles: add theme & posts path

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
-import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/Navbar';
-import Login from './components/Login/Login';
+import Footer from './components/Static/Footer/Footer';
+import Navbar from './components/Static/Navbar/Navbar';
+import Login from './components/User/Login/Login';
 import Register from './components/User/Register/Register';
+import ThemeList from './components/Theme/ThemeList/ThemeList';
+import PostsList from './components/Posts/PostsList/PostsList';
 
 function App() {
   return (
@@ -16,14 +18,25 @@ function App() {
           <Route path='/'>
             <Login />
           </Route>
-          <Route path='/home'>
-            <Home />
-          </Route>
+
           <Route path='/login'>
             <Login />
           </Route>
+
+          <Route path='/home'>
+            <Home />
+          </Route>
+
           <Route path='/register'>
             <Register />
+          </Route>
+
+          <Route path='/themes'>
+            <ThemeList />
+          </Route>
+          
+          <Route path='/posts'>
+            <PostsList />
           </Route>
         </div>
       </Switch>
