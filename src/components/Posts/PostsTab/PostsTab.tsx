fd4049dfed6ddba784d6195d011bea89940f9394
tatialveso <tatiialveso@gmail.com>
2022-02-25 15:52:25 -0300

    :sparkles: complete post tab at home with style

--- src/components/Posts/PostsTab/PostsTab.tsx
+++ src/components/Posts/PostsTab/PostsTab.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { AppBar, Tab, Tabs, Typography, Box } from '@material-ui/core';
 import { TabContext, TabPanel } from '@material-ui/lab';
 import PostsList from '../PostsList/PostsList';
+import './PostsTab.css';
 
 function PostsTab() {
     const [value, setValue] = useState('1');
@@ -12,20 +13,20 @@ function PostsTab() {
   return (
     <>
       <TabContext value={value}>
-        <AppBar position="static">
-          <Tabs centered indicatorColor="secondary" onChange={handleChange}>
-            <Tab label="Todas as postagens" value="1"/>
-            <Tab label="Sobre-nós" value="2" />
+        <AppBar position="static" className="homeTab">
+          <Tabs className="button" centered onChange={handleChange}>
+            <Tab label="Visualizar as postagens" value="1"/>
+            <Tab label="Sobre o blog" value="2" />
           </Tabs>
         </AppBar>
-        <TabPanel value="1" >
+        <TabPanel value="1">
           <Box>
             <PostsList />
           </Box>
         </TabPanel>
         <TabPanel value="2">
-          <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="titulo">Sobre-nós</Typography>
-          <Typography variant="body1" gutterBottom color="textPrimary" align="justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos ut eveniet natus totam et, voluptate dicta tempore alias, odio nobis non eius cupiditate minima inventore pariatur! Ipsum itaque consectetur voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo velit consequuntur suscipit fugiat, nam quis quod quaerat veritatis et, vel ratione beatae, facere neque! Quo animi porro voluptate saepe deleniti? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore adipisci, officia aut quidem dolorum deserunt iure dolorem doloribus velit nobis quas consequatur at ullam odit, nesciunt est nulla nihil excepturi!</Typography>
+          <Typography variant="h5" gutterBottom component="h5" className="aboutTitle">Sobre o blog</Typography>
+          <Typography variant="body1" gutterBottom className="aboutText">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos ut eveniet natus totam et, voluptate dicta tempore alias, odio nobis non eius cupiditate minima inventore pariatur! Ipsum itaque consectetur voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo velit consequuntur suscipit fugiat, nam quis quod quaerat veritatis et, vel ratione beatae, facere neque! Quo animi porro voluptate saepe deleniti? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore adipisci, officia aut quidem dolorum deserunt iure dolorem doloribus velit nobis quas consequatur at ullam odit, nesciunt est nulla nihil excepturi!</Typography>
         </TabPanel>
       </TabContext>
     </>
