b7d5290d06fdf3de086a3d0d9bd39e77ba865dd4
tatialveso <tatiialveso@gmail.com>
2022-02-28 10:19:28 -0300

    :recycle: refactor nav & footer to only show if logged

--- src/components/Static/Footer/Footer.tsx
+++ src/components/Static/Footer/Footer.tsx
@@ -1,37 +1,46 @@
 import React from 'react';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import { Typography, Box, Grid } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokenReducer';
 import './Footer.css';
 
 function Footer() {
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+
+    var footerComponent;
+
+    if (token != "") {
+        footerComponent = <Grid container direction="row" justifyContent="center" alignItems="center" className="footer">
+            <Grid alignItems="center" item xs={12}>
+                <Box>
+                    <Box paddingTop={1}>
+                        <Typography
+                            variant="subtitle2"
+                            align="center"
+                            gutterBottom
+                            style={{ color: "white" }}>
+                            2022 © todos os direitos reservados
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography
+                            variant="subtitle2"
+                            gutterBottom
+                            style={{ color: "white" }}
+                            align="center">
+                            feito com ❤ por <a href="https://tatialveso.github.io" target="_blank">tati alves</a>
+                        </Typography>
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>
+    }
+
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" className="footer">
-                <Grid alignItems="center" item xs={12}>
-                    <Box>
-                        <Box paddingTop={1}>
-                            <Typography
-                                variant="subtitle2"
-                                align="center"
-                                gutterBottom
-                                style={{ color: "white" }}>
-                                2022 © todos os direitos reservados
-                            </Typography>
-                        </Box>
-                        <Box>
-                            <Typography
-                                variant="subtitle2"
-                                gutterBottom
-                                style={{ color: "white" }}
-                                align="center">
-                                feito com ❤ por <a href="https://tatialveso.github.io" target="_blank">tati alves</a>
-                            </Typography>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
+            {footerComponent}
         </>
     )
 }
--- src/components/Static/Navbar/Navbar.tsx
+++ src/components/Static/Navbar/Navbar.tsx
@@ -1,67 +1,79 @@
 import React from 'react';
 import { AppBar, Box, Toolbar, Typography } from '@material-ui/core';
 import { Link, useHistory } from 'react-router-dom';
-import useLocalStorage from 'react-use-localstorage';
+import { useDispatch, useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokenReducer';
+import { addToken } from '../../../store/tokens/actions';
 import './Navbar.css';
 
 function Navbar() {
-    const [token, setToken] = useLocalStorage('token');
     let history = useHistory();
 
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+
+    const dispatch = useDispatch();
+
     function goLogout() {
-        setToken('');
+        dispatch(addToken(''));
         alert('Usuário deslogado');
         history.push('/login');
     }
 
-    return (
-        <>
-            <AppBar position="static" className='navbar'>
-                <Toolbar className="displayFlex" variant="dense">
-                    <Box >
-                        <Typography variant="h5">
-                            BlogPessoal
-                        </Typography>
-                    </Box>
+    var navbarComponent;
+    if (token != "") {
+        navbarComponent = <AppBar position="static" className='navbar'>
+            <Toolbar className="displayFlex" variant="dense">
+                <Box >
+                    <Typography variant="h5">
+                        BlogPessoal
+                    </Typography>
+                </Box>
 
-                    <Box display="flex" justifyContent="start">
-                        <Link to="/home">
-                            <Box mx={1}>
-                                <Typography variant="h6">
-                                    home
-                                </Typography>
-                            </Box>
-                        </Link>
-                        <Link to="/postagens" >
-                            <Box mx={1}>
-                                <Typography variant="h6">
-                                    postagens
-                                </Typography>
-                            </Box>
-                        </Link>
-                        <Link to="/temas" >
-                            <Box mx={1}>
-                                <Typography variant="h6">
-                                    temas
-                                </Typography>
-                            </Box>
-                        </Link>
-                        <Link to="criar-tema">
-                            <Box mx={1}>
-                                <Typography variant="h6">
-                                    cadastrar tema
-                                </Typography>
-                            </Box>
-                        </Link>
-                        <Box mx={1} className="link" onClick={goLogout}>
-                            <Typography className="link" variant="h6" color="inherit">
-                                sair
+                <Box display="flex" justifyContent="start">
+                    <Link to="/home">
+                        <Box mx={1}>
+                            <Typography variant="h6">
+                                home
                             </Typography>
                         </Box>
+                    </Link>
+                    <Link to="/postagens" >
+                        <Box mx={1}>
+                            <Typography variant="h6">
+                                postagens
+                            </Typography>
+                        </Box>
+                    </Link>
+                    <Link to="/temas" >
+                        <Box mx={1}>
+                            <Typography variant="h6">
+                                temas
+                            </Typography>
+                        </Box>
+                    </Link>
+                    <Link to="criar-tema">
+                        <Box mx={1}>
+                            <Typography variant="h6">
+                                cadastrar tema
+                            </Typography>
+                        </Box>
+                    </Link>
+                    <Box mx={1} className="link" onClick={goLogout}>
+                        <Typography className="link" variant="h6" color="inherit">
+                            sair
+                        </Typography>
                     </Box>
+                </Box>
 
-                </Toolbar>
-            </AppBar>
+            </Toolbar>
+        </AppBar>
+    }
+
+    return (
+        <>
+            {navbarComponent}
         </>
     )
 }
